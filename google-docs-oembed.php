98f167c5ce93af0ccad99c8a850ae4ccef8a9a24
Luís R. <goblindegook@users.noreply.github.com>
2015-06-22 12:30:52 +0100

    Check if autoloader exists before requiring

--- google-docs-oembed.php
+++ google-docs-oembed.php
@@ -23,7 +23,9 @@
  * Domain Path:       /languages
  */
 
-require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
 
 use logoscon\GoogleDocsOembed;
 
